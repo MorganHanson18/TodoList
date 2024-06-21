e1288f5fc42343e89c782afed478609aa12ecd6b
apple <apple@MacBook-Pro-di-apple.local>
2024-06-21 13:05:09 +0200

    forms

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,11 +4,13 @@ import{Todo} from '../todo.interface'
 import {TodosService} from '../todos.service'
 import { RouterModule } from '@angular/router';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
 })
@@ -18,9 +20,23 @@ export class DetailsComponent {
   todo = signal<Todo|null>(null);
   todosService = inject(TodosService);
 
+  applyForm = new FormGroup({
+    title: new FormControl(''),
+  });
+
   constructor() {
     this.todosService.getTodo(this.route.snapshot.params['id']).pipe(takeUntilDestroyed()).subscribe((response: Todo) => {
       this.todo.set(response)
     })
+  }
+
+  submitApplication() {
+    const updatedTitle = this.applyForm.value.title ?? '';
+    let current = this.todo()
+    if (current) {
+      current.title = updatedTitle;
+      this.todo.set(current)
+      this.todosService.submitApplication(updatedTitle, current.id);
+    }
   }
 }
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -16,4 +16,10 @@ export class TodosService {
     return this.http.get<Todo>('https://jsonplaceholder.typicode.com/todos' + '/' + id)
   }
 
+  submitApplication(title: string, id: number) {
+    console.log(
+      `Title change received: title: ${title}`,
+    );
+  }
+
 }
